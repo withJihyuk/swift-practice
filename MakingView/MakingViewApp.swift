8c0651e44c769a047e0c91a6b7f952a096fae7fe
Jihyuk Lee <lead@sver.dev>
2024-06-19 19:45:03 +0900

    Initial Commit

---
+++ MakingView/MakingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingViewApp.swift
+//  MakingView
+//
+//  Created by dlwlgur on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MakingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
